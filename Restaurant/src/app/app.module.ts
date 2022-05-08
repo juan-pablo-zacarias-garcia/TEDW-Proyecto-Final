bf3eed0a86430986ed528d33357256686ad87339
juan-pablo-zacarias-garcia <zacarias.garcia.juanpablo.1j@gmail.com>
2022-05-08 11:47:47 -0500

    Se adaptó la plantilla, se agregaron los archivos de estilo css y js

--- Restaurant/src/app/app.module.ts
+++ Restaurant/src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './users/navbar/navbar.component';
 import { FooterComponent } from './users/footer/footer.component';
+import { OfferComponent } from './users/offer/offer.component';
+import { ClientsComponent } from './users/clients/clients.component';
+import { BookComponent } from './users/book/book.component';
+import { AboutComponent } from './users/about/about.component';
+import { FoodComponent } from './users/food/food.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    OfferComponent,
+    ClientsComponent,
+    BookComponent,
+    AboutComponent,
+    FoodComponent 
   ],
   imports: [
     BrowserModule,
